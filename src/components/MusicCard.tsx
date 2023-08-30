5d8be92595e2f2ad413d557b7412276267510a19
Wagner Lopes <wagnerlopesbr@hotmail.com>
2023-08-30 13:42:59 -0300

    WL Project TrybeTunes 5/5 v1 (requisito 3 e 4 ainda não aprovaram no github mesmo tendo aprovado no teste interno; requisito 5 funcionando porém ainda não aprovado no teste interno)

--- src/components/MusicCard.tsx
+++ src/components/MusicCard.tsx
@@ -1,10 +1,14 @@
+import { useState } from 'react';
 import { SongType } from '../types';
+import checked_heart from '../images/checked_heart.png';
+import empty_heart from '../images/empty_heart.png';
 
 type MusicsProps = {
   song: SongType;
 };
 
 function MusicCard({ song }: MusicsProps) {
+  const [favorited, setFavorited] = useState(false);
   return (
     <div key={ song.trackId }>
       <h3>{song.trackName}</h3>
@@ -15,6 +19,27 @@ function MusicCard({ song }: MusicsProps) {
         <code>audio</code>
         .
       </audio>
+      <div>
+        <label
+          data-testid={ `checkbox-music-${song.trackId}` }
+          htmlFor={ `favorited-${song.trackId}` }
+        >
+          {
+            favorited
+              ? (<img src={ checked_heart } alt="favorite" />)
+              : (<img src={ empty_heart } alt="favorite" />)
+          }
+        </label>
+        <input
+          data-testid={ `favorited-${song.trackId}` }
+          checked={ favorited }
+          onChange={ () => {
+            setFavorited(!favorited);
+            console.log(favorited);
+          } }
+          type="checkbox"
+        />
+      </div>
     </div>
   );
 }
